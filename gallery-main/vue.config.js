3a8e40063311946cfca45a1df416728166f9b230
haeun <shy624@naver.com>
2023-01-16 10:07:04 +0900

    manifest, vue config

---
+++ gallery-main/vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  pwa: {
+    workboxOptions: {
+      runtimeCaching:[{
+        urlPattern: /\.png$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'png-cache',
+          expiration: {
+            maxEntries: 10, // 총 파일 10개까지 캐시
+            maxAgeSeconds: 60 * 60 * 24 * 365, // 1년 캐시
+          }
+        }
+      },
+      {
+        urlPattern:/\.json$/,
+        handler: 'StleWhileRevalidate',
+        options: {
+          cacheName:'icon-cache',
+          cacheableResponse: {
+            status:[200]
+          }
+        },
+      }
+    ],
+  }
+},
+
+transpileDependencies: [
+  ''
+]
