1544862e12f8d033c2fd00fe87ee3ef426738ffa
haeun <shy624@naver.com>
2023-02-06 17:47:03 +0900

    edit error

--- rct_todos/src/App.js
+++ rct_todos/src/App.js
@@ -1,24 +1,104 @@
 // 파이어베이스에서 db 가져오기
-import { db } from './firebase';
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from 'react';
+import { db } from './firebase'
+import { collection, doc, getDocs, addDoc, updateDoc, deleteDoc, query, orderBy } from "firebase/firestore";
+import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const todosCollectionRef = collection(db, "todos");
+  const [changed, setChanged] = useState(false)
 
-  useEffect( () => {
-    const getLists = async() => {
-      const data = await getDocs(todosCollectionRef)
-      console.log(data)
-    
+  const [newList, setNewList] = useState("");
+  // console.log(newList)
+
+  const [todos, setList] = useState([])
+  const todosCollectionRef = collection(db, "todos");
+  
+  useEffect( () =>{
+    const getLists = async () => {
+      const data = await getDocs(
+        query(todosCollectionRef, orderBy("timeStamp", "desc"))
+      )
+      // console.log(data)
+      setList( 
+        data.docs.map( 
+          (doc)=>(
+            { ...doc.data(), id:doc.id}
+          )
+        )
+      )
     }
-  })
-  getLists();
+    
+    getLists();
+    setChanged(false)
+  }, [changed])
+  
+  const date = new Date();
+  const now_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+
+  const createList = async () => {
+    await addDoc(todosCollectionRef, 
+      {
+        content:newList, 
+        d_date:now_date,
+        timeStamp:date
+      }
+    )
+    setChanged(true)
+  }
+
+  const updateList = async (id, content)=> {
+    // console.log(id + "/" + content);
+    const msg = window.prompt("TO DO", content)
+
+    if(msg){
+      // id를 이용하여 업데이트 할 데이터 검색
+      const listDoc = doc(db, 'todos', id)
+      // 업데이트 할 데이터
+      const editField = {
+        content:msg, 
+        d_date:now_date,
+        timeStamp:new Date()
+      }
+      // updateDoc(어떤 데이터, 어떤 값) 데이터 업데이트
+      await updateDoc(listDoc, editField)
+      setChanged(true)
+    }
+  }
+
+  const deleteList = async (id) => {
+    const cfm = window.confirm("Are you sure?");
+
+    if(cfm){
+      // id를 이용하여 삭제할 데이터 검색
+      const listDoc = doc(db, 'todos', id)
+      // deleteDoc(어떤 데이터, 어떤 값) 데이터 업데이트
+      await deleteDoc(listDoc)
+    }
+    setChanged(true)
+  }
+
+  const showList = todos.map(
+    (value) => (
+      <div key={value.id}>
+        <h2>
+          {value.content}
+          <span className='date'>{value.d_date}</span>
+          <button onClick={ ()=>{ updateList(value.id, value.content) } }>EDIT</button>
+          <button onClick={ ()=>{ deleteList(value.id) } }>DELETE</button>
+        </h2>
+      </div>
+    )
+  )
 
   return (
-    <div>
-
+    <div className="App">
+      <input type="text" placeholder='todos...' onChange={
+        (event)=> {setNewList(event.target.value)}
+        } />
+        <button onClick={createList}>Add List</button>
+      <hr />
+      {showList}
     </div>
   );
 }
--- rct_todos/src/firebase.js
+++ rct_todos/src/firebase.js
@@ -1,17 +1,16 @@
+// 필요한 기능 가져오기
 import { initializeApp } from "firebase/app";
-import { getFirebasestore } from '@firebase/firestore';
+import {getFirestore} from "@firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlqn5_QVJYT3XHvt29d7ut8_blIJAYgQw",
-  authDomain: "react-project-92abd.firebaseapp.com",
-  projectId: "react-project-92abd",
-  storageBucket: "react-project-92abd.appspot.com",
-  messagingSenderId: "435583578100",
-  appId: "1:435583578100:web:1a7defc4d23d435c13d3a1"
-};
+    apiKey: "AIzaSyAlqn5_QVJYT3XHvt29d7ut8_blIJAYgQw",
+    authDomain: "react-project-92abd.firebaseapp.com",
+    projectId: "react-project-92abd",
+    storageBucket: "react-project-92abd.appspot.com",
+    messagingSenderId: "435583578100",
+    appId: "1:435583578100:web:1a7defc4d23d435c13d3a1"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig)
 
-export const db = getFirebasestore(app);
+  export const db = getFirestore(app);
